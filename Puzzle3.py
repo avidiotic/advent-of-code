1d150ed90a03bd772b8515f2e6d0a6c9b46e755a
Avi Agarwal <avidiotic@users.noreply.github.com>
2019-06-27 18:21:48 +0530

    Completed Puzzle 3

--- Puzzle3.py
+++ Puzzle3.py
@@ -1,30 +1,25 @@
 import re
 from collections import defaultdict
 from itertools import product
-import time
-
 
 data = open('Puzzle3_input.txt', 'r').read().split('\n')
 measurements = (map(int, re.findall(r'-?\d+', line)) for line in data)
 
 cloth_map = defaultdict(list)
-cloth_map_2 = defaultdict(list)
+overlap = defaultdict(list)
 
-start = time.time()
 for claim_no, coord_x, coord_y, w, h in measurements:
-    
-    for i in range(coord_x, coord_x+w):
-        for j in range(coord_y, coord_y+h):
-            cloth_map[(i,j)].append(claim_no)
-
-print(sum(1 if len(cloth_map[key])>1 else 0 for key in cloth_map))
-print(f'Nested loops took {time.time() - start}')
-
-start = time.time()
-for claim_no, coord_x, coord_y, w, h in measurements:
-    
+    overlap[claim_no] = set()
     for sq in product(range(coord_x, coord_x+w), range(coord_y, coord_y+h)):
-        cloth_map_2[sq].append(claim_no)
-    
-print(sum(1 if len(cloth_map_2[key])>1 else 0 for key in cloth_map_2))
-print(f'Product took {time.time() - start}')
+        if cloth_map[sq]:
+            for existing_claim in cloth_map[sq]:
+                overlap[claim_no].add(existing_claim)
+                overlap[existing_claim].add(claim_no)
+        cloth_map[sq].append(claim_no)
+
+
+print(sum(1 if len(cloth_map[key]) > 1 else 0 for key in cloth_map))
+# Solution of Part 1 = 121259
+
+print([key for key in overlap if len(overlap[key]) == 0][0])
+# Solution of Part 2 = 239
